3c33aae9c258b531089871567c39e02734a4cc9f
talljohnthin <eljustin12@gmail.com>
2019-12-29 15:05:07 +0800

    up

--- src/components/WishList/Index.js
+++ src/components/WishList/Index.js
@@ -3,20 +3,47 @@ import { Container } from 'react-bootstrap'
 import { WishListContext } from '../../contexts/WishListContext'
 import { Link } from 'react-router-dom'
 import Product from './Product'
+import firebase from '../../config/firebase'
+import notification from './../Helpers/functions'
 
 export default function Index() {
     const [wish, setWish] = useState([])
     const [total, setTotal] = useState(0)
+    const [currentUserName, setCurrentUserName] = useState(null)
+    const [currentUserPhone, setCurrentUserPhone] = useState(null)
+    const [currentUserAddress, setCurrentUserAddress] = useState(null)
     const { wishListState, wishListDispatch } = useContext(WishListContext)
     
     useEffect(()=>{
         setWish(wishListState.products)
         getTotal()
+        console.log(firebase.auth().currentUser && firebase.auth().currentUser.phoneNumber)
     }, [wishListState])
 
     const getTotal = () => {
         const totalResult = wishListState.products.reduce((acc, item) => item.total + acc, 0)
         setTotal(totalResult)
+    }
+
+    const getCurrentUserInfo = () => {
+        if (firebase.auth().currentUser) {
+            setCurrentUserName(firebase.auth().currentUser.displayName)
+            setCurrentUserPhone(firebase.auth().currentUser.phoneNumber)
+        } else {
+            notification('Wish', 'You must be logged in to continue. Thank you!', 'danger')
+        }
+        
+    }
+
+    const handleOrder = () => {
+        if ( firebase.auth().currentUser) {
+            const userId = firebase.auth().currentUser.uid
+            const phone = firebase.auth().currentUser.phoneNumber
+            const userName = firebase.auth().currentUser.displayName
+
+        } else {
+            notification('Wish', 'You must be logged in to order. Thank you!', 'danger')
+        }
     }
 
 
@@ -40,8 +67,8 @@ export default function Index() {
             <div className="WISHLIST-product-total">
             <div className="title">Total</div>
                 <ul>
-                    <li><span className="label">Total :</span><span class="amount">{total}</span></li>
-                    <li><span className="label">Shipping :</span><span class="amount">Added later</span></li>
+                    <li><span className="label">Total :</span><span className="amount">{total}</span></li>
+                    <li><span className="label">Shipping :</span><span className="amount">Added later</span></li>
                 </ul>
             </div>
 
@@ -51,23 +78,26 @@ export default function Index() {
                             
                 <div className="form-check">
                     <input className="form-check-input" name="user-info" type="checkbox" />
-                    <label className="form-check-label" for="user">
+                    <label className="form-check-label">
                         Use current login information
                     </label>
                 </div>
                     <div className="each">
                         <div className="label">Ship to:</div>
-                        <div className="form-group"><input type="text" name="name" placeholder="Input name" className="form-control"/></div>
+                        <div className="form-group"><input type="text" name="name" placeholder="Input name" value={currentUserName} className="form-control"/></div>
                     </div>
                     <div className="each">
                         <div className="label">Shipping Address:</div>
-                        <div className="form-group"><input type="text" name="address" placeholder="Input address" className="form-control"/></div>
+                        <div className="form-group"><input type="text" name="address" placeholder="Input address" value={currentUserAddress} className="form-control"/></div>
                     </div>
                     <div className="each">
                         <div className="label">Phone/Mobile Number:</div>
-                        <div className="form-group"><input type="number" name="phone" placeholder="Input phone" className="form-control"/></div>
+                        <div className="form-group"><input type="number" name="phone" placeholder="Input phone" value={currentUserPhone} className="form-control"/></div>
                     </div>
                 </form>
+            </div>
+            <div className="WISHLIST-submit">
+                <span className="btn btn-primary" onClick={handleOrder}>Order Now</span>
             </div>
         </Container>
         </div>
